fa2af36ef261d35aa98e9637a6752002774e5fce
YanTelles <48263268+YanTelles@users.noreply.github.com>
2021-06-22 21:24:59 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
-const token="b29d76b4f8e10e8e4dac241113cbb1caf20fc8c4";
-
-const filters = {state: "TO", is_last: "False"}; 
-
-const url=`https://api.brasil.io/v1/dataset/covid19/caso/data?state=${filters.state}&is_last=${filters.is_last}`
+const button = document.getElementById('btn');
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -13,35 +9,52 @@ function mobileMenu() {
     navMenu.classList.toggle("active");
 }
 
-// Lembrar de atualizar a chave de autenticação
+document.getElementById('btn').addEventListener('click', function(event) {
+  event.preventDefault();
+  let city = document.getElementById('city').value;
+  let cityConverted = PrimeiraMaiuscula(city);
+  let state = document.getElementById('estados').value;
+  let is_last = 'True'; 
+  const url=`https://api.brasil.io/v1/dataset/covid19/caso/data?state=${state}&city=${cityConverted}&is_last=${is_last}`;
+  const token='b29d76b4f8e10e8e4dac241113cbb1caf20fc8c4';
 
-fetch(
-  url,
-  {
-    method: 'get',
-    headers: {
-      Authorization: `Token ${token}`,
-    },  
-  }
+  fetch(
+    url,
+    {
+      method: 'get',
+      headers: {
+        Authorization: `Token ${token}`,
+      },  
+    }
+  
+  ).then(res => res.json())
+  .then(function(resposta){
+    
+    console.log(resposta);
 
-).then(res => res.json())
-.then(function(resposta){
+    let taxa = resposta.results[0].death_rate;
+    document.querySelector('#taxa').innerHTML = (taxa.toFixed(2)*100)+'%';  
 
+    let confirmados = resposta.results[0].confirmed;
+    document.querySelector('#confirmados').innerHTML = confirmados.toLocaleString('pt-BR');
 
-  let taxa = resposta.results[0].death_rate;
-  document.querySelector('#taxa').innerHTML = (taxa.toFixed(2)*100)+'%';  
+    let population = resposta.results[0].estimated_population;
+    document.querySelector('#populacao').innerHTML = population.toLocaleString('pt-BR');
 
-  let confirmados = resposta.results[0].confirmed;
-  document.querySelector('#confirmados').innerHTML = confirmados.toLocaleString('pt-BR');
+    let fatalidades = resposta.results[0].deaths;
+    document.querySelector('#fatalidades').innerHTML = fatalidades.toLocaleString('pt-BR');
 
-  let population = resposta.results[0].estimated_population;
-  document.querySelector('#populacao').innerHTML = population.toLocaleString('pt-BR');
+    let data = resposta.results[0].date;
+    document.querySelector('#data').innerHTML = data;
 
-  let fatalidades = resposta.results[0].deaths;
-  document.querySelector('#fatalidades').innerHTML = fatalidades.toLocaleString('pt-BR');
+    let city = resposta.results[0].city;
+    document.querySelector('#resumo').innerHTML = `Resumo ${city}`;
 
-  let data = resposta.results[0].date;
-  document.querySelector('#data').innerHTML = data;
+  }); 
 });
 
+function PrimeiraMaiuscula(texto) {
+  var substituto = texto.toLowerCase().replace(/(?:^|\s)\S/g, function(PrimeiraLetra) { return PrimeiraLetra.toUpperCase(); });
+  return substituto;
+}
 
